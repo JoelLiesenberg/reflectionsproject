eb692630165e97a1b41bc85ca930191a7301eade
JoelLiesenberg <99089723+JoelLiesenberg@users.noreply.github.com>
2022-12-14 15:22:43 -0500

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,7 @@
+// function showFootnote(note) {
+//     alert(note)
+// }
+
+// document.getElementById('note1').onclick = showFootnote(note='The Watcher is a character tasked by Hydaelyn to sit on the moon and watch over Zodiark in his cage, as well as keep an eye on the world.')
+
+<span class="footnote" onclick="javascript:alert('');"></span>
